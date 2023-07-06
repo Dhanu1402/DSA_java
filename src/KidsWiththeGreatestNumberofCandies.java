2650676f9ef54e40f9a662d6ab4fdbbafe6e4d82
dhanu1402 <dhanukumar2000@gmail.com>
2023-07-06 22:56:32 +0530

    KidsWiththeGreatestNumberofCandies

---
+++ src/KidsWiththeGreatestNumberofCandies.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class KidsWiththeGreatestNumberofCandies {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = candies[0];
+
+        for (int i = 1; i < candies.length; i++){
+            if(candies[i] > max){
+                max = candies[i];
+            }
+        }
+
+        List<Boolean> output = new ArrayList<>();
+
+        for (int i = 0; i < candies.length; i++){
+            if(candies[i] + extraCandies < max){
+                output.add(false);
+            } else {
+                output.add(true);
+            }
+        }
+
+        return output;
+    }
+}
